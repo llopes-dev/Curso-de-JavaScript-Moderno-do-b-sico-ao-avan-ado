265d54bb9fcd33ca84643fd550e8c598b0eca1c5
Leandro Lopes <leandro.lopes.dev@gmail.com>
2019-12-02 16:28:09 -0300

    constantes

---
+++ basico/aula7/index.js
@@ -0,0 +1,14 @@
+// não podemos criar constantes com palavras reservadas;
+// varáveis precisam ter nomes significativos;
+// não pode começar o nome de uma varável com um número;
+// não podem conter espaços;
+// utilizamos camelCase;
+// não podemos modificar o valor de uma constante;
+// não utilize var, utilize LET;
+
+const primeiroNumero = 5;
+const segundoNumero = 10;
+
+const conta = primeiroNumero * segundoNumero;
+
+console.log(conta)
