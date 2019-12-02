6c138f9d3233a5d2333e01be73d0896e9b1139bb
Leandro Lopes <leandro.lopes.dev@gmail.com>
2019-12-02 15:54:54 -0300

    varivaeis let

---
+++ basico/aula6/index.js
@@ -0,0 +1,23 @@
+// não podemos criar variáveis com palavras reservadas;
+// varáveis precisam ter nomes significativos;
+// não pode começar o nome de uma varável com um número;
+// não podem conter espaços;
+// utilizamos camelCase;
+
+
+let  nome = 'João';
+
+
+console.log( nome, 'nasceu em 1984');
+console.log('Em 2000'  , nome , 'conheceu Maria.');
+console.log( nome , 'casou-se com Maria em 2012');
+console.log('Maria teve 1 filho com' , nome , 'em 2015');
+console.log('O filho de' , nome , 'se chama Eduardo.');
+
+
+
+console.log( '------------------------------' );
+
+let nome; // declarou a variavel
+nome = 'Qualquer valor'; // Inicializando a variavel
+console.log(nome);
