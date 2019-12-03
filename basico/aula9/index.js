9a923214165f00644482d9877b6f5ed69e5119ed
Leandro Lopes <leandro.lopes.dev@gmail.com>
2019-12-03 10:20:07 -0300

    dados primitivos

---
+++ basico/aula9/index.js
@@ -0,0 +1,11 @@
+// Tipos de Dados Primitivos
+
+const nome = 'Leandro'; // string
+const nome2 = "Leandro"; // string
+const nome3 = `Leandro` // string
+
+const num1 = 10; // number
+const num2 = 10.52 // number
+let nomeAluno; // undefined = não aponta para local nenhum na memória
+const sonbrenomeAluno; // Nulo -> não aponta para local nenhum na memória
+const aprovado = false ; // Valores booleanos pode ser true or false
