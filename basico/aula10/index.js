ed536bc2df57e0ab734b2af58e6294764a57a28e
Leandro Lopes <leandro.lopes.dev@gmail.com>
2019-12-03 11:18:06 -0300

    alert, confirm e prompt (Navegador)

---
+++ basico/aula10/index.js
@@ -0,0 +1,10 @@
+/* Operadores Aritméticos
+
++ Adição e Concatenização
+- Subtração
+* Multiplicação
+/ Divisão 
+% resto da divisão
+incremento = ++
+decremento = --
+*/
---
+++ basico/aula11/js/index.js
@@ -0,0 +1,6 @@
+const num1 = prompt('digite um numero');
+const num2 = prompt('digie o segundo numero');
+
+soma = parseInt(num1) + parseInt(num2);
+
+alert('A soma dos número é ' + soma);
