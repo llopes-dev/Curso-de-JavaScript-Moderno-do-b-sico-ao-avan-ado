47643d5d2a8f4504045294151ae2c9cf65c5feb1
Leandro Lopes <leandro.lopes.dev@gmail.com>
2019-12-03 16:07:17 -0300

    curso de array

---
+++ basico/aula18/index.js
@@ -0,0 +1,13 @@
+// indice, inicia do 0 e segue 1 , 2 ...
+const alunos = ['Maria' , 'Luiz' , 'João']; 
+
+alunos.unshift('Luiza');
+alunos.unshift('Fábio');
+
+// alunos.push('Luiza'); // Adiciona no fim
+// alunos.push('Fábio'); // Adiciona no fim
+
+// alunos[0] = 'Eduardo' // Altera
+// alunos[3] = 'Luiza'; // Adiciona
+
+console.log(alunos)
