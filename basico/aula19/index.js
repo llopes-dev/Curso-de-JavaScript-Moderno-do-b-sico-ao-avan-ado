0f4249a25445f2e286ac41eac480ba85e2456fe1
Leandro Lopes <leandro.lopes.dev@gmail.com>
2019-12-04 18:24:04 -0300

    objetos (basico)

---
+++ basico/aula19/index.js
@@ -0,0 +1,33 @@
+// const array = [1,2,3];
+// array.push(4);
+// array[0] = 'Leandro';
+// console.log(array)
+
+
+// // outro Exemplo com Objeto
+// const pessoa1 = {
+
+//         nome: 'Luiz',
+//         sobrenome: 'Miranda',
+//         idade: 25
+// };
+
+// console.log(pessoa1.nome);
+
+// função Factory -> função que cria objeto
+
+// function criaPessoa(nome, sobrenome, idade){
+//     return{nome ,  sobrenome, idade};
+// }
+
+// const pessoa1 = criaPessoa('Luiz' , 'Otávio' , 25);
+// const pessoa2 = criaPessoa('Leandro' , 'da Silva' , 36)
+
+// console.log(pessoa1.nome);
+
+
+const pessoa1 = {
+
+    
+
+};
