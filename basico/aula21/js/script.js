fc48b30630fb5a76c2f0875d0cebd941ac660fbe
Leandro Lopes <leandro.lopes.dev@gmail.com>
2019-12-05 09:59:46 -0300

    ajuste em aula de função, array e objetos

--- basico/aula21/js/script.js
+++ basico/aula21/js/script.js
@@ -1,12 +1,14 @@
 function meuEscopo(){
     const form = document.querySelector('.form');
 
-        let contador = 1;
+       
 
         function recebeEventoForm (evento){
-            evento.preventDefault();
-            console.log(`Form não foi enviado ${contador}`);
-            contador++;
+            const nome = form.querySelector('.nome');
+            const sobrenome = form.querySelector('.sobrenome');
+            const peso = form.querySelector('.peso');
+            const altura = form.querySelector('.altura');
+            
         }
 
         form.addEventListener('submit' , recebeEventoForm);
