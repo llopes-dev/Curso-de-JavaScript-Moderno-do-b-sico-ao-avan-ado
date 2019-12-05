b82c68240e2a09c1c341794f77b5ab29761d280b
Leandro Lopes <leandro.lopes.dev@gmail.com>
2019-12-05 10:51:26 -0300

    Operadores Lógicos

---
+++ 2 - Logica de Programação/2 - Operadores Lógicos/index.js
@@ -0,0 +1,6 @@
+/**
+ * Operadores Lógico
+ * && -> END -> E -> todas as expressões precisa ser verdadeiras para retornar true
+ * || -> OR -> OU -> uma das expressões precisa ser verdadeiras para retornar true
+ * ! -> NOT -> NÃO
+ */
