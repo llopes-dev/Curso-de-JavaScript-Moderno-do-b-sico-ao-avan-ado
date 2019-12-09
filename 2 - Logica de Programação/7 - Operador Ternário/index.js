26461ce8916e7bfb817fddbb5b1672e336e67ae6
Leandro Lopes <leandro.lopes.dev@gmail.com>
2019-12-09 14:34:13 -0300

    operador ternario

---
+++ 2 - Logica de Programação/7 - Operador Ternário/index.js
@@ -0,0 +1,5 @@
+// (condicao) ? 'valor para Verdadeiro': 'valor para falso'
+
+const pontuacaoUsuario = 1000;
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuário VIP' : 'Usuário Normal';
+console.log(nivelUsuario);
